78ed2af8e3eb53c9d002a051e7606bee9e22a271
Anna Popova <anna.popova201909@gmail.com>
2024-05-27 12:30:03 +0300

    поправила быструю сортировку

--- src/sprint3/finall/Solution_B.java
+++ src/sprint3/finall/Solution_B.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Arrays;
 
 public class Solution_B {
     public static void main(String[] args) throws IOException {
@@ -13,7 +13,7 @@ public class Solution_B {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             final int participantsNumber = Integer.parseInt(reader.readLine());
 
-            for (int i = 0; i <participantsNumber; i++) {
+            for (int i = 0; i < participantsNumber; i++) {
                 final String[] currParticipant = reader.readLine().trim().split(" ");
                 score.add(Integer.parseInt(currParticipant[1]));
             }
@@ -22,30 +22,35 @@ public class Solution_B {
         quicksort(score, 0, score.size() - 1);
     }
 
-    public static int partition(ArrayList<Integer> array, int leftIndex, int rightIndex, int pivot) {
-        while (leftIndex < rightIndex) {
-            if (array.get(leftIndex) > pivot && array.get(rightIndex) < pivot) {
-                int temp = array.get(leftIndex);
-                array.set(leftIndex, array.get(rightIndex));
-                array.set(rightIndex, temp);
-            }
-            leftIndex++;
-            rightIndex--;
-        }
+    public static void swap(ArrayList<Integer> array, int leftIndex, int rightIndex) {
+        int temp = array.get(leftIndex);
+        array.set(leftIndex, array.get(rightIndex));
+        array.set(rightIndex, temp);
+    }
 
+
+    public static int partition(ArrayList<Integer> array, int leftIndex, int rightIndex) {
+        int pivot = array.get(rightIndex);
+        int rightCopy = rightIndex;
+        while (leftIndex < rightCopy) {
+            while (leftIndex < rightCopy && array.get(leftIndex) < pivot) {
+                leftIndex++;
+            }
+            while (leftIndex < rightCopy && array.get(rightCopy) >= pivot) {
+                rightCopy--;
+            }
+            swap(array, leftIndex, rightCopy);
+        }
+        swap(array, leftIndex, rightIndex);
+        System.out.println(Arrays.toString(array.toArray()));
         return leftIndex;
     }
 
-    public static ArrayList<Integer> quicksort(ArrayList<Integer> array, int leftIndex, int rightIndex) {
-        if (rightIndex-leftIndex <=1) {
-            return array;
-        } else {
-            Random random = new Random();
-            int pivot = array.get(random.nextInt(array.size()));
-            final int newLeftIndex = partition(array, leftIndex, rightIndex, pivot);
-            quicksort(array, 0, newLeftIndex);
-            quicksort(array, newLeftIndex, array.size() - 1);
+    public static void quicksort(ArrayList<Integer> array, int leftIndex, int rightIndex) {
+        if (leftIndex < rightIndex) {
+            final int newLeftIndex = partition(array, leftIndex, rightIndex);
+            quicksort(array, leftIndex, newLeftIndex - 1);
+            quicksort(array, newLeftIndex, rightIndex);
         }
-        return array;
     }
 }
