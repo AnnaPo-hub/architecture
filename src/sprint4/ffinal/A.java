8324bf8b5a131377e97510a5f341e370bec9386a
Anna Popova <anna.popova201909@gmail.com>
2024-06-06 14:56:42 +0300

    sprint 4 add A

--- src/sprint4/ffinal/A.java
+++ src/sprint4/ffinal/A.java
@@ -12,31 +12,76 @@ import java.util.stream.Collectors;
 public class A {
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            final HashMap<Integer, List<Integer>> vocabulary =
+            final HashMap<String, List<Integer>> vocabulary =
                     buildVocabulary(Integer.parseInt(reader.readLine()), reader);
 
+            final int requestQuantity = Integer.parseInt(reader.readLine());
 
-            System.out.println(vocabulary.entrySet());
+            for (int i = 0; i < requestQuantity; i++) {
+
+                findThat(readListOfString(reader), vocabulary);
+            }
+
         }
     }
 
+    private static List<Integer> findThat(List<String> request, HashMap<String, List<Integer>> vocabulary) {
+        final ArrayList<String> checkedWord = new ArrayList<>();
+        final ArrayList<Integer> result = new ArrayList<>();
 
-    private static HashMap<Integer, List<Integer>> buildVocabulary(int documentQuantity, BufferedReader reader) throws IOException {
-        final HashMap<Integer, List<Integer>> vocabulary = new HashMap<>();
-        for (int i = 0; i <= documentQuantity; i++) {
+        final HashMap<Integer, Integer> occurrenceMap = new HashMap<>();
+
+        for (String currWord : request) {
+            if (checkedWord.contains(currWord)) {
+
+            } else {
+                checkedWord.add(currWord);
+
+                System.out.println("currWord is " + currWord);
+             //   final List<Integer> listOfDocs = vocabulary.get(currWord.hashCode());
+                //получаем вхождения в документ [1, 2, 2],
+                final List<Integer> listOfDocs = vocabulary.get(currWord);
+
+                if (listOfDocs == null) {
+                    return null;
+                } else {
+
+                    for (int i = 0; i < listOfDocs.size(); i++) {
+                        if (occurrenceMap.containsKey(listOfDocs.get(i))) {
+                            final Integer integer = occurrenceMap.get(listOfDocs.get(i));
+                            occurrenceMap.put(listOfDocs.get(i), integer+ 1);
+                        } else {
+                            occurrenceMap.put(listOfDocs.get(i), 1);
+                        }
+                    }
+                    System.out.println(
+                            occurrenceMap.entrySet()
+                    );
+                }
+
+            }
+        }
+        return result;
+    }
+
+    private static HashMap<String, List<Integer>> buildVocabulary(int documentQuantity, BufferedReader reader) throws IOException {
+        final HashMap<String, List<Integer>> vocabulary = new HashMap<>();
+        for (int i = 0; i < documentQuantity; i++) {
+            int lineNumber = i + 1;
             final List<String> document = readListOfString(reader);
             for (String word : document) {
 
                 final int hash = word.hashCode();
-                if (vocabulary.containsKey(hash)) {
-                    vocabulary.get(hash).add(++i);
+                if (vocabulary.containsKey(word)) {
+                    vocabulary.get(word).add(lineNumber);
                 } else {
                     ArrayList<Integer> objects = new ArrayList<>();
-                    objects.add(++i);
-                    vocabulary.put(hash, objects);
+                    objects.add(lineNumber);
+                    vocabulary.put(word, objects);
                 }
             }
         }
+        System.out.println(vocabulary.entrySet());
         return vocabulary;
     }
 
