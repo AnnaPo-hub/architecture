66caa3523fe72d4aafe30fe6ab04b2873eaee397
Anna Popova <anna.popova201909@gmail.com>
2024-06-07 11:15:54 +0300

    обновила B спринт 4

--- src/sprint4/ffinal/B.java
+++ src/sprint4/ffinal/B.java
@@ -8,8 +8,18 @@ public class B {
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             final int requestQuantity = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < requestQuantity; i++) {
+                final String[] command = reader.readLine().trim().split(" ");
 
-
+                switch (command[0]) {
+                    case "get":
+                        break;
+                    case "put":
+                        break;
+                    case "delete":
+                        break;
+                }
+            }
         }
     }
 }
