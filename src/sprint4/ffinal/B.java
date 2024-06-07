ac0136504533787b1c1879847fe397791a62b32c
Anna Popova <anna.popova201909@gmail.com>
2024-06-07 10:59:22 +0300

    добавила задачу B спринт 4

---
+++ src/sprint4/ffinal/B.java
@@ -0,0 +1,15 @@
+package sprint4.ffinal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            final int requestQuantity = Integer.parseInt(reader.readLine());
+
+
+        }
+    }
+}
