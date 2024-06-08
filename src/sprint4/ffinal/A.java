91bb11818e63ddd78ce149334000494c907f759d
Anna Popova <anna.popova201909@gmail.com>
2024-06-08 14:56:21 +0300

    revised final sprint 4

--- src/sprint4/ffinal/A.java
+++ src/sprint4/ffinal/A.java
@@ -1,78 +1,84 @@
 package sprint4.ffinal;
+    /*
+-- ПРИНЦИП РАБОТЫ --
+Используемая структура данных : хеш-таблица
+Проходим в цикле по каждому слову из каждого  документа,
+получаем хэш и складываем в хеш-таблицу в следующем  виде:  ключ - хэш слова , значение в свою очередь в виде хеш-таблицы.
+Ключ - номер документа, значение - количество вхождений в этот документ. Это позволяет ускорить обработку результатов, когда слово встречается несколько раз в документе.
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+В цикле проходим поисковые запросы, проходим каждое уникальное  слово,
+затем получаем  его хэш и по хэшу данные из "словаря".
+Сортируем данные в соответствии с указанной в задаче логикой релевантности и выводим результат.
+
+-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+Для получания хэша мы пользуемся  хеш-таблицами из стандартных библиотек, поэтому не сомневаемся в том, что найдем элемент по
+хэшу там где его положили.
+Получаем хэш, складываем данные в хеш-таблицу, далее  по хэшу слова из поискового запроса находим данные по вхождениям.
+Мы можем быть уверены, что если слова одинаковые, то хэш у них также одинаковый и мы найдем количество вхождений слова по хэшу.
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+Сложность построения словаря О(n), где n количество слов по всем документам, которое в итоге внесем в таблицу.
+Сложность поиска  в среднем О(n), где n количество слов в запросе.
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+O(n)  памяти, где n - это  количество корзин.
+
+--ID успешной посылки--
+https://contest.yandex.ru/contest/24414/run-report/115048412/
+     */
+
+import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class A {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            final HashMap<String, List<Integer>> vocabulary =
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            final HashMap<String, HashMap<Integer, Integer>> vocabulary =
                     buildVocabulary(Integer.parseInt(reader.readLine()), reader);
 
             final int requestQuantity = Integer.parseInt(reader.readLine());
-
             for (int i = 0; i < requestQuantity; i++) {
-                System.out.println(getSortedResult(findThat(readListOfString(reader), vocabulary)));
+                String request = reader.readLine();
+                String requestResult = "";
+
+                List<Map.Entry<Integer, Integer>> res = getSortedResult(search(readSetOfString(request), vocabulary));
+                for (int j = 0; j < Math.min(5, res.size()); j++) {
+                    Map.Entry<Integer, Integer> entry = res.get(j);
+                    requestResult = requestResult + entry.getKey() + " ";
+                }
+                writer.write(requestResult);
+                writer.newLine();
             }
         }
     }
 
-    private static HashMap<Integer, Integer> findThat(List<String> request, HashMap<String, List<Integer>> vocabulary) {
-        final ArrayList<String> checkedWord = new ArrayList<>();
-
+    private static HashMap<Integer, Integer> search(HashSet<String> request, HashMap<String, HashMap<Integer, Integer>> vocabulary) {
         final HashMap<Integer, Integer> occurrenceMap = new HashMap<>();
 
         for (String currWord : request) {
-            if (checkedWord.contains(currWord)) {
-
-            } else {
-                checkedWord.add(currWord);
-
-                //         System.out.println("currWord is " + currWord);
-                //   final List<Integer> listOfDocs = vocabulary.get(currWord.hashCode());
-                //получаем вхождения в документ [1, 2, 2],
-                final List<Integer> listOfDocs = vocabulary.get(currWord);
-
-                if (listOfDocs!=null) {
-
-                    for (int i = 0; i < listOfDocs.size(); i++) {
-                        if (occurrenceMap.containsKey(listOfDocs.get(i))) {
-                            final Integer integer = occurrenceMap.get(listOfDocs.get(i));
-                            occurrenceMap.put(listOfDocs.get(i), integer + 1);
-                        } else {
-                            occurrenceMap.put(listOfDocs.get(i), 1);
-                        }
-                    }
-                    //         System.out.println(
-                    //         occurrenceMap.entrySet());
-                }
-            }
+            final HashMap<Integer, Integer> listOfDocs = vocabulary.get(currWord);
+            if (listOfDocs != null)
+                listOfDocs.forEach((docNumber, count) -> occurrenceMap.merge(docNumber, count, Integer::sum));
         }
         return occurrenceMap;
     }
 
-
-    private static HashMap<String, List<Integer>> buildVocabulary(int documentQuantity, BufferedReader reader) throws IOException {
-        final HashMap<String, List<Integer>> vocabulary = new HashMap<>();
-        for (int i = 0; i < documentQuantity; i++) {
-            int lineNumber = i + 1;
+    private static HashMap<String, HashMap<Integer, Integer>> buildVocabulary(int documentQuantity, BufferedReader reader) throws IOException {
+        final HashMap<String, HashMap<Integer, Integer>> vocabulary = new HashMap<>();
+        for (int i = 0; i < documentQuantity; ++i) {
             final List<String> document = readListOfString(reader);
             for (String word : document) {
-
-                final int hash = word.hashCode();
                 if (vocabulary.containsKey(word)) {
-                    vocabulary.get(word).add(lineNumber);
+                    vocabulary.get(word).merge(i + 1, 1, Integer::sum);
                 } else {
-                    ArrayList<Integer> objects = new ArrayList<>();
-                    objects.add(lineNumber);
+                    HashMap<Integer, Integer> objects = new HashMap<>();
+                    objects.put(i + 1, 1);
                     vocabulary.put(word, objects);
                 }
             }
         }
-        //      System.out.println(vocabulary.entrySet());
         return vocabulary;
     }
 
@@ -80,12 +86,18 @@ public class A {
 
         return resultMap.entrySet()
                 .stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue()) == 0 ?
+                        o1.getKey().compareTo(o2.getKey()) : (o2.getValue().compareTo(o1.getValue())))
                 .collect(Collectors.toList());
     }
 
     private static List<String> readListOfString(BufferedReader reader) throws IOException {
         return Arrays.stream(reader.readLine().split(" "))
                 .collect(Collectors.toList());
+    }
+
+    private static HashSet<String> readSetOfString(String string) throws IOException {
+        return (HashSet<String>) Arrays.stream(string.split(" "))
+                .collect(Collectors.toCollection(HashSet::new));
     }
 }
--- src/sprint4/ffinal/HashTable.java
+++ src/sprint4/ffinal/HashTable.java
@@ -1,4 +1,37 @@
 package sprint4.ffinal;
+    /*
+-- ПРИНЦИП РАБОТЫ --
+Используемая структура данных : динамический массив
+Для каждого ключа вычисляется хэш, получаем индекс позиции путем вычисления остатка от деления хэша
+на количество корзин.  Для  разрешения  коллиций используется линейное пробирование: если получаем такой же хэш,
+который уже был внесен в таблицу, то ищем ближайшую свободную корзину и размещаем там элемент.
+
+put —–   по индексу  элемента кладем элемент в таблицу.
+
+get key –— по индексу идет поиск в массиве.
+
+delete key –— помечаем  элемент по индексу как удаленный. Это необходимо для корректного получения элементов,
+которые были размещены после текущего элемента при возникновении коллизии.
+
+-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+Всегда можем по ключу получить то, что сохранили, тк хэш одного и того же слова всегда одинаковый.
+Тк для разрешения коллизий мы использовали линейное пробирование, то при получении элемента по хэшу мы дополнительно
+проверили правильность его значения по ключу.
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+put —–  О(1), если сразу попадаем в пустой бакет. O(n), где n - это количество  корзин
+
+get key –—  В лучшем случае О(1), если сразу нашли нужную корзину или  в худшем случае O(n), где n - это количество
+бакетов, которые нужно перебрать
+
+delete key –— аналогично предыдущим методам, O(1) в лучшем случае или O(n) в худшем случае
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+O(n)  памяти, где n - это  количество корзин.
+
+--ID успешной посылки--
+https://contest.yandex.ru/contest/24414/run-report/115054528/
+     */
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,39 +42,34 @@ public class HashTable<K, V> {
 
     private ArrayList<Node<K, V>> bucketArray;
     private int bucketQuantity;
-    //  private int size;
-
 
     public HashTable(int bucketQuantity) {
         bucketArray = new ArrayList<>();
         this.bucketQuantity = bucketQuantity;
 
-        //заполняет бакеты null
         for (int i = 0; i < bucketQuantity; i++) {
             bucketArray.add(null);
         }
     }
 
     private int getIndex(K key) {
-        return key.hashCode() % bucketQuantity;
+        return Math.floorMod(key.hashCode(), bucketQuantity);
     }
 
     private void put(K key, V value) {
         int index = getIndex(key);
-        while (bucketArray.get(index) != null) {
-            index++;
-            index %= bucketQuantity;
+        while (bucketArray.get(index) != null && !bucketArray.get(index).isDeleted() && !bucketArray.get(index).isKeyEquals(key)) {
+            index = Math.floorMod(++index, bucketQuantity);
         }
-        bucketArray.add(getIndex(key), new Node(key, value));
+        bucketArray.set(index, new Node(key, value));
     }
 
     private V get(K key) {
         int index = getIndex(key);
         while (bucketArray.get(index) != null) {
-            if (bucketArray.get(index).key == key)
+            if (bucketArray.get(index).isKeyEquals(key))
                 return bucketArray.get(index).getValue();
-            index++;
-            index %= bucketQuantity;
+            index = Math.floorMod(++index, bucketQuantity);
         }
         return null;
     }
@@ -49,13 +77,13 @@ public class HashTable<K, V> {
     private V delete(K key) {
         int index = getIndex(key);
         while (bucketArray.get(index) != null) {
-            if (bucketArray.get(index).key == key) {
-                Node<K, V> temp = bucketArray.get(index);
-                bucketArray.set(index, null);
+            Node<K, V> temp = bucketArray.get(index);
+            if (temp.isKeyEquals(key)) {
+                V res = temp.value;
+                temp.setDeleted();
                 return temp.getValue();
             }
-            index++;
-            index %= bucketQuantity;
+            index = Math.floorMod(++index, bucketQuantity);
         }
         return null;
     }
@@ -64,7 +92,7 @@ public class HashTable<K, V> {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             final int requestQuantity = Integer.parseInt(reader.readLine());
 
-            final HashTable<Integer, Integer> salaryHashTable = new HashTable<>(113);
+            final HashTable<Integer, Integer> salaryHashTable = new HashTable<>(426389);
 
             for (int i = 0; i < requestQuantity; i++) {
                 final String[] command = reader.readLine().trim().split(" ");
@@ -87,9 +115,23 @@ public class HashTable<K, V> {
     }
 
     public static class Node<K, V> {
-        public K key;
+        private K key;
         public V value;
-        public Node<K, V> nextNode;
+        private boolean isDeleted = false;
+
+        public boolean isDeleted() {
+            return isDeleted;
+        }
+
+        public boolean isKeyEquals(K anotherKey) {
+            return key != null && key.equals(anotherKey);
+        }
+
+        public void setDeleted() {
+            isDeleted = true;
+            key = null;
+            return;
+        }
 
         public Node(K key, V value) {
             this.key = key;
