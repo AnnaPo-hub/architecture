5be4dbc608fe72ab59365b95a1615dc4cc2a185b
Anna Popova <anna.popova201909@gmail.com>
2024-06-17 17:36:17 +0300

    обновила B спринт

--- src/sprint5/training/E2.java
+++ src/sprint5/training/E2.java
@@ -2,26 +2,29 @@ package sprint5.training;
 
 public class E2 {
     public static boolean treeSolution(Node head) {
-        return isSearchTree(head);
+        return isSearchTree(head, 1, Integer.MAX_VALUE);
     }
 
-    public static boolean isSearchTree(Node head) {
+    public static boolean isSearchTree(Node head, int minValue, int maxValue) {
         if ((head.right != null && head.left != null)) {
-            if (head.right.value <= head.value || head.left.value >= head.value) {
+            if (head.right.value <= head.value || head.left.value >= head.value ||
+                    head.value > maxValue ||
+                    head.value < minValue) {
                 return false;
             } else if (head.right.value > head.value && head.left.value < head.value) {
-                return isSearchTree(head.right) && isSearchTree(head.left);
+                return isSearchTree(head.right, head.value, maxValue ) &&
+                        isSearchTree(head.left, minValue , head.value);
             }
         } else if (head.left != null) {
-            if (head.left.value < head.value) {
-                return isSearchTree(head.left);
+            if (head.left.value < head.value && head.left.value < maxValue) {
+                return isSearchTree(head.left, head.value, maxValue);
             } else {
                 return false;
             }
 
-        } else if (head.right != null){
-            if (head.right.value > head.value) {
-                return isSearchTree(head.right);
+        } else if (head.right != null) {
+            if (head.right.value > head.value && head.right.value > minValue) {
+                return isSearchTree(head.right, minValue, head.value);
             } else {
                 return false;
             }
@@ -52,14 +55,14 @@ public class E2 {
         node0.right = node2;
 
         Node node3 = new Node(1);
-        Node node4 = new Node(3);
-
-        node1.left =  node3;
-        node1.right =  node4;
+        Node node4 = new Node(4);
+//
+        node1.left = node3;
+        node1.right = node4;
         Node node5 = new Node(6);
         Node node6 = new Node(9);
-        node2.right =  node6;
-        node2.left =  node5;
+        node2.right = node5;
+        node2.left = node6;
         System.out.println(treeSolution(node0));
     }
 }
