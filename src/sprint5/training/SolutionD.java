af49b550669158f47e53c137e7bb53ccd0f67248
Anna Popova <anna.popova201909@gmail.com>
2024-06-19 12:18:10 +0300

    обновила 5 спринт задача D

--- src/sprint5/training/SolutionD.java
+++ src/sprint5/training/SolutionD.java
@@ -3,24 +3,23 @@ package sprint5.training;
 public class SolutionD {
     public static boolean treeSolution(Node head1, Node head2) {
         // Your code
-     return    compareTrees(head1, head2);
+        return compareTrees(head1, head2);
     }
 
     private static boolean compareTrees(Node head1, Node head2) {
-        if(head1.value!= head2.value){
+        if (head1.value != head2.value) {
             return false;
         }
         if (doNotHaveChildren(head1, head2)) {
             return true;
         } else if (haveTwoSameChildren(head1, head2)) {
-            compareTrees(head1.right, head2.right);
-            compareTrees(head1.left, head2.left);
+            return compareTrees(head1.right, head2.right) && compareTrees(head1.left, head2.left);
         } else if (haveLeftSameChildren(head1, head2)) {
-            compareTrees(head1.left, head2.left);
+            return compareTrees(head1.left, head2.left);
         } else if (haveRightSameChildren(head1, head2)) {
-            compareTrees(head1.right, head2.right);
+            return compareTrees(head1.right, head2.right);
         }
-        return true;
+        return false;
     }
 
     private static boolean haveTwoSameChildren(Node head1, Node head2) {
@@ -65,9 +64,9 @@ public class SolutionD {
         Node node0 = new Node(0, node1, null);
 
 
-      //  Node node5 = new Node(3, null, null);
-        Node node4 = new Node(1, null, null);
-        Node node3 = new Node(0, node4, null);
+        //  Node node5 = new Node(3, null, null);
+        // Node node4 = new Node(1, null, null);
+        Node node3 = new Node(0, null, null);
         System.out.println(treeSolution(node3, node0));
         //     assert treeSolution(node3, node6);
     }
