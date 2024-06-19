9b6bdee014d3ea213c1c4145a8dd9c626b839c9f
Anna Popova <anna.popova201909@gmail.com>
2024-06-19 10:11:18 +0300

    обновила D sprint 5

--- src/sprint5/training/SolutionD.java
+++ src/sprint5/training/SolutionD.java
@@ -3,20 +3,24 @@ package sprint5.training;
 public class SolutionD {
     public static boolean treeSolution(Node head1, Node head2) {
         // Your code
-        compareTrees(head1, head2);
-        return true;
+     return    compareTrees(head1, head2);
     }
 
     private static boolean compareTrees(Node head1, Node head2) {
-        if (haveSameChildren(head1, head2)) {
+        if(head1.value!= head2.value){
+            return false;
+        }
+        if (doNotHaveChildren(head1, head2)) {
+            return true;
+        } else if (haveTwoSameChildren(head1, head2)) {
             compareTrees(head1.right, head2.right);
             compareTrees(head1.left, head2.left);
+        } else if (haveLeftSameChildren(head1, head2)) {
+            compareTrees(head1.left, head2.left);
+        } else if (haveRightSameChildren(head1, head2)) {
+            compareTrees(head1.right, head2.right);
         }
         return true;
-    }
-
-    private static boolean haveSameChildren(Node head1, Node head2) {
-        return haveTwoSameChildren(head1, head2) || haveLeftSameChildren(head1, head2) || haveRightSameChildren(head1, head2);
     }
 
     private static boolean haveTwoSameChildren(Node head1, Node head2) {
@@ -56,15 +60,15 @@ public class SolutionD {
     // <template>
 
     public static void main(String[] args) {
-
-
+        //Node node2 = new Node(3, null, null);
         Node node1 = new Node(1, null, null);
-        Node node2 = new Node(2, null, null);
-        Node node3 = new Node(3, node1, node2);
+        Node node0 = new Node(0, node1, null);
+
+
+      //  Node node5 = new Node(3, null, null);
         Node node4 = new Node(1, null, null);
-        Node node5 = new Node(2, null, null);
-        Node node6 = new Node(3, node4, node5);
-        System.out.println(treeSolution(node3, node6));
-   //     assert treeSolution(node3, node6);
+        Node node3 = new Node(0, node4, null);
+        System.out.println(treeSolution(node3, node0));
+        //     assert treeSolution(node3, node6);
     }
 }
