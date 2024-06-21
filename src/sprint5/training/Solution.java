0187468d8c18362e45a95c4a7b5a58564d456de8
Anna Popova <anna.popova201909@gmail.com>
2024-06-21 10:07:13 +0300

    Добавила L sprint 5

---
+++ src/sprint5/training/Solution.java
@@ -0,0 +1,14 @@
+package sprint5.training;
+
+public class Solution {
+    public static int siftDown(int[] heap, int idx) {
+        // Your code
+        // “ヽ(´▽｀)ノ”
+        return 0;
+    }
+
+    private static void test() {
+        int[] sample = {-1, 12, 1, 8, 3, 4, 7};
+        assert siftDown(sample, 2) == 5;
+    }
+}
