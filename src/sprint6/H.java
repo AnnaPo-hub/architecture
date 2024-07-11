07b29ddced678eb1db2b37ef4c98282d31e2761c
Anna Popova <anna.popova201909@gmail.com>
2024-07-11 17:44:32 +0300

    обновила 6 спринт H

---
+++ src/sprint6/H.java
@@ -0,0 +1,21 @@
+package sprint6;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class H {
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+        }
+    }
+
+    private static List<Integer> readList(BufferedReader reader) throws IOException {
+        return Arrays.stream(reader.readLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}
