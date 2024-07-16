72c8c8fd271ef8ba9e11efba8a926390dd3ff8d6
Anna Popova <anna.popova201909@gmail.com>
2024-07-16 08:14:17 +0300

    обновила 6 спринт J

--- src/sprint6/J.java
+++ src/sprint6/J.java
@@ -44,26 +44,40 @@ public class J {
 
     }
 
-    private List<Integer> outgoingEdges(Integer vertex, ArrayList<Integer>[] vectors) {
-        Collections.sort(vectors[vertex], Collections.reverseOrder());
-        return vectors[vertex];
+//    private List<Integer> outgoingEdges(Integer vertex, ArrayList<Integer>[] vectors) {
+//        Collections.sort(vectors[vertex], Collections.reverseOrder());
+//        return vectors[vertex];
+//    }
+
+    private List<Integer> outgoingEdges(Integer vertex) {
+        List<Integer> outgoingEdges = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[vertex][j] == 1 && !outgoingEdges.contains(j)) {
+                    outgoingEdges.add(j);
+                }
+            }
+        }
+        Collections.sort(outgoingEdges, Collections.reverseOrder());
+        // System.out.println(" Возвращаю  исходящие смежные вершины для вершины " + vertex);
+        //  outgoingEdges.forEach(System.out::print);
+        return outgoingEdges;
     }
 
-    //возвращает массив списков смежных вершин для неориентированного графа
-    ArrayList<Integer>[] getEdgesData(int edgesQuantity, BufferedReader reader, int vectorQuantity) throws IOException {
-        ArrayList<Integer>[] vectors = new ArrayList[vectorQuantity + 1];
-
-        for (int i = 1; i < vectors.length; i++) {
-            vectors[i] = new ArrayList<>();
+    //возвращает входящие ребра
+    private List<Integer> ingoingEdges(Integer vertex) {
+        List<Integer> ingoingEdges = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][vertex] == 1 && !ingoingEdges.contains(i)) {
+                    ingoingEdges.add(i);
+                }
+            }
         }
-
-        for (int i = 0; i < edgesQuantity; i++) {
-            final List<Integer> currentLine = readList(reader);
-            final Integer firstVector = currentLine.get(0);
-            final Integer secondVector = currentLine.get(1);
-            vectors[firstVector].add(secondVector);
-        }
-        return vectors;
+        Collections.sort(ingoingEdges, Collections.reverseOrder());
+        // System.out.println(" Возвращаю входящие смежные вершины для вершины " + vertex);
+        //  ingoingEdges.forEach(System.out::print);
+        return ingoingEdges;
     }
 
     private boolean hasIncomingNeigbors(int vector, ArrayList<Integer>[] vectors) {
@@ -71,7 +85,7 @@ public class J {
     }
 
 
-    void DFS(int startVertex, ArrayList<Integer>[] vectors) {
+    void DFS(int startVertex) {
 
         Stack<Integer> stack = new Stack<>();
         stack.push(startVertex);  // Добавляем стартовую вершину в стек.
@@ -82,36 +96,38 @@ public class J {
             // Это может быть как новая вершина, так и уже посещённая однажды.
             int v = stack.pop();
 
-            final List<Integer> neighbors = outgoingEdges(v, vectors);
-//            if (neighbors.isEmpty()) {
-//                //эта вершина не имеет смежных вершин
-//                color.set(v, (byte) 2);
-//                single.push(v);
-//            } else {
-            if (color.get(v) == 0) {
-                // Красим вершину в серый. И сразу кладём её обратно в стек:
-                // это позволит алгоритму позднее вспомнить обратный путь по графу.
-                color.set(v, (byte) 1);
-                //System.out.println(" Печатаю вершину " + v);
-                //   System.out.print(v + " ");
-                stack.push(v);
-
-                // Теперь добавляем в стек все непосещённые соседние вершины,
-                // вместо вызова рекурсии
-
-                for (int w : neighbors) {
-                    // Для каждого исходящего ребра (v, w):
-                    if (color.get(w) == 0) {
-                        stack.push(w);
-                    }
-                }
-            } else if (color.get(v) == 1) {
-                // Серую вершину мы могли получить из стека только на обратном пути.
-                // Следовательно, её следует перекрасить в чёрный.
+            final List<Integer> outgoing = outgoingEdges(v);
+            final List<Integer> incoming = ingoingEdges(v);
+            if (incoming.isEmpty() && outgoing.isEmpty()) {
+                //эта вершина не имеет смежных вершин
                 color.set(v, (byte) 2);
-                //   leave[v] = time;// Запишем время выхода.
-                order.push(v);
-                //    time += 1;
+                single.push(v);
+            } else {
+                if (color.get(v) == 0) {
+                    // Красим вершину в серый. И сразу кладём её обратно в стек:
+                    // это позволит алгоритму позднее вспомнить обратный путь по графу.
+                    color.set(v, (byte) 1);
+                    //System.out.println(" Печатаю вершину " + v);
+                    //   System.out.print(v + " ");
+                    stack.push(v);
+
+                    // Теперь добавляем в стек все непосещённые соседние вершины,
+                    // вместо вызова рекурсии
+
+                    for (int w : outgoing) {
+                        // Для каждого исходящего ребра (v, w):
+                        if (color.get(w) == 0) {
+                            stack.push(w);
+                        }
+                    }
+                } else if (color.get(v) == 1) {
+                    // Серую вершину мы могли получить из стека только на обратном пути.
+                    // Следовательно, её следует перекрасить в чёрный.
+                    color.set(v, (byte) 2);
+                    //   leave[v] = time;// Запишем время выхода.
+                    order.push(v);
+                    //    time += 1;
+                }
             }
         }
     }
@@ -130,20 +146,19 @@ public class J {
 
             c.initializeColor(vectorQuantity);
 
-            final ArrayList<Integer>[] edgesData = c.getEdgesData(edgesQuantity, reader, vectorQuantity);
 
             for (int i = 1; i < c.color.size(); i++) {
                 // Перебираем варианты стартовых вершин, пока они существуют.
                 if (c.color.get(i) == 0) {
-                    c.DFS(i, edgesData); // Запускаем обход, стартуя с i-й вершины.
+                    c.DFS(i); // Запускаем обход, стартуя с i-й вершины.
                 }
             }
 
-            for (int i = 0; i <= c.single.size(); i++) {
+            for (int i = c.single.size(); i > 0; i--) {
                 System.out.print(c.single.pop() + " ");
             }
 
-            for (int i = 0; i <= c.order.size(); i++) {
+            for (int i = c.order.size(); i > 0; i--) {
                 System.out.print(c.order.pop() + " ");
             }
         }
