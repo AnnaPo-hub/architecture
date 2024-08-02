5de51e6b6a3777c4aa268aa5149dcbb4bcafbf59
Anna Popova <anna.popova201909@gmail.com>
2024-08-02 17:07:07 +0300

    обновила 7 спринт B

--- src/sprint7/B.java
+++ src/sprint7/B.java
@@ -7,9 +7,8 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
-
-//class Lesson {
 class Lesson implements Comparable<Lesson> {
     double start;
     double end;
@@ -19,110 +18,82 @@ class Lesson implements Comparable<Lesson> {
         this.end = end;
     }
 
-    public double getStart() {
-        return start;
-    }
-
-    public double getEnd() {
-        return end;
-    }
-
     @Override
     public String toString() {
-        DecimalFormat format = new DecimalFormat("0.#");
+        DecimalFormat format = new DecimalFormat("0.##");
         return format.format(start) + " " + format.format(end);
     }
 
     @Override
     public int compareTo(Lesson lesson) {
-        return Double.compare(start, lesson.start);
+        if (this.end < lesson.end)
+            return -1;
+        else if (this.end > lesson.end)
+            return 1;
+        else {
+            if (this.start < lesson.start)
+                return -1;
+            else if (this.start > lesson.start)
+                return 1;
+
+        }
+        return 0;
     }
 }
 
+
 public class B {
 
-
     ArrayList<Lesson> schedule;
+    ArrayList<Lesson> lessons;
     int counter;
     Lesson previousLesson;
 
-    ArrayList<Double> starts;
-    ArrayList<Double> ends;
-
     public B() {
         this.schedule = new ArrayList<>();
+        lessons = new ArrayList<>();
         counter = 0;
         previousLesson = null;
-        starts = new ArrayList<>();
-        ends = new ArrayList<>();
     }
 
-    private boolean isStartOk(double start) {
-        for (int i = 0; i < starts.size(); i++) {
-            if (start > starts.get(i) && start < ends.get(i))
-                return false;
+    private void readAndSaveAllLessons(BufferedReader reader, int lessonsQuantity) throws IOException {
+        for (int i = 0; i < lessonsQuantity; i++) {
+            final double[] hours = readList(reader);
+            lessons.add(new Lesson(hours[0], hours[1]));
         }
-        return true;
     }
 
-    private boolean isEndOk(double end) {
-        for (int i = 0; i < starts.size(); i++) {
-            if (end > starts.get(i) && end <= ends.get(i))
-                return false;
-
+    private void createSchedule(List<Lesson> lessons) {
+        for (Lesson currLesson : lessons) {
+            if (schedule.isEmpty()) {
+                schedule.add(currLesson);
+                previousLesson = currLesson;
+                ++counter;
+            } else if (currLesson.start >= previousLesson.end) {
+                schedule.add(currLesson);
+                previousLesson = currLesson;
+                ++counter;
+            }
         }
-        return true;
-    }
-
-
-    //каждый урок  пара  чисел
-    // при получении очередного урока проверить, что  внутри его границ нет других уроков
-    private boolean isSpaceOk(Lesson lesson) {
-        double start = lesson.getStart();
-        double end = lesson.getEnd();
-
-        for (int i = 0; i < starts.size(); i++) {
-            if (starts.get(i) >= start && ends.get(i) <= end)
-                return false;
-        }
-        return true;
-    }
-
-
-    private boolean addToSchedule(Lesson lesson) {
-        if (isEndOk(lesson.end) && isStartOk(lesson.start) && isSpaceOk(lesson)) {
-            //  if (isEndOk(lesson.end) && isStartOk(lesson.start)) {
-            schedule.add(lesson);
-            starts.add(lesson.start);
-            ends.add(lesson.end);
-            return true;
-        }
-        return false;
     }
 
 
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             final int lessonsQuantity = Integer.parseInt(reader.readLine());
-            createSchedule(reader, lessonsQuantity);
+
+            B b = new B();
+            b.readAndSaveAllLessons(reader, lessonsQuantity);
+            Collections.sort(b.lessons);
+
+            b.createSchedule(b.lessons);
+
+            System.out.println(b.counter);
+
+            Collections.sort(b.schedule);
+            b.schedule.forEach(System.out::println);
 
         }
-    }
-
-    private static void createSchedule(BufferedReader reader, int lessonsQuantity) throws IOException {
-        B b = new B();
-
-        for (int i = 0; i < lessonsQuantity; i++) {
-            final double[] hours = readList(reader);
-            final boolean isAdded = b.addToSchedule(new Lesson(hours[0], hours[1]));
-            if (isAdded)
-                ++b.counter;
-        }
-
-        System.out.println(b.counter);
-
-        Collections.sort(b.schedule);
-        b.schedule.forEach(System.out::println);
     }
 
 
