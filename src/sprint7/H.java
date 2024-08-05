da6a8a73d9110268252c720cd0a271ac40b7c9fb
Anna Popova <anna.popova201909@gmail.com>
2024-08-05 22:51:54 +0300

    обновила 7 спринт H

--- src/sprint7/H.java
+++ src/sprint7/H.java
@@ -18,9 +18,9 @@ public class H {
 
     //заполняем поле цветочками
     //+добавляем один дополнительный ряд снизу и один ряд слева в качестве каемочки
-    private void initializeMatrix(BufferedReader reader, int n, int m) throws IOException {
+    private void initializeMatrixMirror(BufferedReader reader, int n, int m) throws IOException {
 
-        for (int i = 0; i < n; i++) {
+        for (int i = n; i > 0; i--) {
 
             final List<Integer> currLine = readLine(reader);
 
@@ -34,29 +34,7 @@ public class H {
         }
 
         for (int j = 0; j <= m; j++) {
-            flowers[n][j] = Integer.MIN_VALUE;
-        }
-    }
-
-    //заполняем поле цветочками
-    //+добавляем один дополнительный ряд снизу и один ряд слева в качестве каемочки
-    private void initializeMatrixMirror(BufferedReader reader, int n, int m) throws IOException {
-
-        for (int i = n - 1; i >= 0; i--) {
-
-            final List<Integer> currLine = readLine(reader);
-
-            for (int j = 0; j <= m; j++) {
-                if (j == 0) {
-                    flowers[i][j] = Integer.MIN_VALUE;
-                } else {
-                    flowers[i][j] = currLine.get(Math.abs(m - j));
-                }
-            }
-        }
-
-        for (int j = 0; j <= m; j++) {
-            flowers[n][j] = Integer.MIN_VALUE;
+            flowers[0][j] = Integer.MIN_VALUE;
         }
     }
 
@@ -65,7 +43,7 @@ public class H {
             for (int j = 1; j <= m; j++) {
                 //базовый случай
                 //на первом шаге мы возьмём столько фишек, сколько есть в стартовой ячейке
-                // dp[i][j] = flowers[i][j];
+                //dp[i][j] = flowers[i][j];
                 dp[i][j] = Integer.max(dp[i - 1][j], dp[i][j - 1] + flowers[i][j]);
             }
         }
@@ -79,9 +57,8 @@ public class H {
             final Integer m = matrixSize.get(1);
 
             H h = new H(n, m);
-            h.initializeMatrix(reader, n, m);
+            h.initializeMatrixMirror(reader, n, m);
 
-            System.out.println("matrix is ready ");
 
             h.countFlowersMax(n, m);
 
