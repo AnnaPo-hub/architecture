c94b2e6b5a85d89e6aa2766cb8642fa7d918c093
Anna Popova <anna.popova201909@gmail.com>
2024-08-05 22:14:48 +0300

    обновила 6 спринт E

--- src/sprint7/H.java
+++ src/sprint7/H.java
@@ -20,11 +20,11 @@ public class H {
     //+добавляем один дополнительный ряд снизу и один ряд слева в качестве каемочки
     private void initializeMatrix(BufferedReader reader, int n, int m) throws IOException {
 
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = 0; i < n; i++) {
 
             final List<Integer> currLine = readLine(reader);
 
-            for (int j = m - 1; j >= 0; j--) {
+            for (int j = 0; j <= m; j++) {
                 if (j == 0) {
                     flowers[i][j] = Integer.MIN_VALUE;
                 } else {
@@ -38,15 +38,36 @@ public class H {
         }
     }
 
+    //заполняем поле цветочками
+    //+добавляем один дополнительный ряд снизу и один ряд слева в качестве каемочки
+    private void initializeMatrixMirror(BufferedReader reader, int n, int m) throws IOException {
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            final List<Integer> currLine = readLine(reader);
+
+            for (int j = 0; j <= m; j++) {
+                if (j == 0) {
+                    flowers[i][j] = Integer.MIN_VALUE;
+                } else {
+                    flowers[i][j] = currLine.get(Math.abs(m - j));
+                }
+            }
+        }
+
+        for (int j = 0; j <= m; j++) {
+            flowers[n][j] = Integer.MIN_VALUE;
+        }
+    }
+
     private void countFlowersMax(int n, int m) {
-        for (int i = n; i >= 0; i--) {
-            for (int j = 0; j < m; j++) {
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 1; j <= m; j++) {
                 //базовый случай
                 //на первом шаге мы возьмём столько фишек, сколько есть в стартовой ячейке
                 // dp[i][j] = flowers[i][j];
                 dp[i][j] = Integer.max(dp[i - 1][j], dp[i][j - 1] + flowers[i][j]);
             }
-
         }
     }
 
