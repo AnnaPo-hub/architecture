d671a378eda8e8bba13587827a939fbd55953019
Anna Popova <anna.popova201909@gmail.com>
2024-08-08 11:55:26 +0300

    обновила 7 спринт L

--- src/sprint7/L.java
+++ src/sprint7/L.java
@@ -13,8 +13,16 @@ public class L {
     int[][] dp;
 
 
-    public L(int n, int goldBarQuantity) {
-        this.dp = new int[n + 1][goldBarQuantity + 1];
+    public L(int goldBarQuantity, int k) {
+        this.dp = new int[goldBarQuantity + 1][k + 1];
+        for (int i = 0; i <= goldBarQuantity; i++) {
+            for (int j = 0; j <= k; j++) {
+                if (j == 0 || i == 0 || (j == 0 && i == 0)) {
+                    dp[i][j] = -1;
+                }
+            }
+
+        }
 
     }
 
@@ -28,23 +36,23 @@ public class L {
 //слитки с номерами от 0 до i
             final List<Integer> barWeights = readList(reader);
             Collections.sort(barWeights);
-            final L l = new L(j, goldBarQuantity);
-            for (int i = 1; i < goldBarQuantity; i++) {
-                for (int k = 1; k < j; k++) {
+            final L l = new L(goldBarQuantity, j);
+            for (int i = 1; i <= goldBarQuantity; i++) {
+                for (int k = 1; k <= j; k++) {
                     l.dp[0][0] = 0;
                     // максимум из (предыдущий максимум или  вес текущего элемента   + оставшийся свободный  вес в рюкзаке)
                     // вес текущего элемента
                     int a = barWeights.get(i - 1);
-                    System.out.println("вес текущего элемента" + a);
+                    System.out.println("вес текущего элемента " + a);
                     //то, что поместится в оставшемся  свободном месте   в рюкзаке
-                    int b = l.dp[i - 1][k - barWeights.get(i - 1)];
-                    System.out.println("оставшийся свободный  вес в рюкзаке" + b);
-                    l.dp[i][k] = Integer.max(l.dp[i - 1][k], a + b);
+                    int b = k - barWeights.get(i - 1) <= 0 ? 0 : a + l.dp[i - 1][k - barWeights.get(i - 1)];
+                    System.out.println("b = " + b);
+
+                    System.out.println("запишу как  текущий максимум " + Integer.max(l.dp[i - 1][k], a + b));
+                    l.dp[i][k] = Integer.max(l.dp[i - 1][k], b);
                 }
-
             }
-
-            System.out.println(l.dp[j + 1][goldBarQuantity + 1]);
+            System.out.println(l.dp[goldBarQuantity + 1][j + 1]);
         }
     }
 
