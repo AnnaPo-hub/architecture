f27ff5a800c2632d9b7f1fd198cec5351185150c
Anna Popova <anna.popova201909@gmail.com>
2024-08-12 12:02:59 +0300

    обновила 7 спринт Final B

--- src/sprint7/ffinal/B.java
+++ src/sprint7/ffinal/B.java
@@ -7,14 +7,7 @@ import java.util.Arrays;
 
 public class B {
     //храним набор половины суммы
-    int[][] dp;
-
-
-    public B(int gameQuantity, int k) {
-        this.dp = new int[gameQuantity + 1][k + 1];
-//        for (int i = 0; i <= gameQuantity; i++) {
-//        }
-    }
+    static short[][] dp;
 
     //Одним из параметров динамики будет половина суммы массива, которую будем набирать
     public static void main(String[] args) throws IOException {
@@ -30,14 +23,13 @@ public class B {
                 int half = Arrays.stream(scoreList).sum() / 2;
 
 
-                B b = new B(gameQuantity, half);
-
+                B b = new B();
+                dp = new short[gameQuantity + 1][half + 1];
                 for (int i = 0; i < gameQuantity; i++) {
-
                     for (int k = 0; k <= half; k++) {
 
                         //предыдущий максимум
-                        int prevMax = i - 1 < 0 ? 0 : b.dp[i - 1][k];
+                        int prevMax = i - 1 < 0 ? 0 : dp[i - 1][k];
 
                         // текущий элемент массива
                         int currElement = scoreList[i];
@@ -45,17 +37,17 @@ public class B {
                         int x = k - currElement;
                         if (x < 0) {
 
-                            b.dp[i][k] = prevMax;
+                            dp[i][k] = (short) prevMax;
                         } else if (x == 0) {
-                            b.dp[i][k] = currElement;
+                            dp[i][k] = (short) currElement;
                         } else {
                             //то, что поместится в оставшемся  свободном месте   в рюкзаке
                             int y = k - currElement < 0 || i - 1 <= 0 ? 0 : b.dp[i - 1][k - currElement];
-                            b.dp[i][k] = Integer.max(prevMax, currElement + y);
+                            dp[i][k] = (short) Integer.max(prevMax, currElement + y);
                         }
                     }
                 }
-                System.out.println(b.dp[gameQuantity - 1][half] == half ? "True" : "False");
+                System.out.println(dp[gameQuantity - 1][half] == half ? "True" : "False");
             }
         }
     }
