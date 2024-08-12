5832e754ac4b1ac3404647c0afd6302025db646f
Anna Popova <anna.popova201909@gmail.com>
2024-08-12 12:28:34 +0300

    обновила 7 спринт Final B

--- src/sprint7/ffinal/B.java
+++ src/sprint7/ffinal/B.java
@@ -14,24 +14,17 @@ public class B {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             final int gameQuantity = Integer.parseInt(reader.readLine());
 
-//числа из массива с номерами от 0 до i
             int[] scoreList = readList(reader);
             int sum = Arrays.stream(scoreList).sum();
             if (sum % 2 != 0) {
                 System.out.println("False");
             } else {
                 int half = Arrays.stream(scoreList).sum() / 2;
-
-
-                B b = new B();
                 dp = new short[gameQuantity + 1][half + 1];
                 for (int i = 0; i < gameQuantity; i++) {
                     for (int k = 0; k <= half; k++) {
 
-                        //предыдущий максимум
                         int prevMax = i - 1 < 0 ? 0 : dp[i - 1][k];
-
-                        // текущий элемент массива
                         int currElement = scoreList[i];
 
                         int x = k - currElement;
@@ -41,8 +34,7 @@ public class B {
                         } else if (x == 0) {
                             dp[i][k] = (short) currElement;
                         } else {
-                            //то, что поместится в оставшемся  свободном месте   в рюкзаке
-                            int y = k - currElement < 0 || i - 1 <= 0 ? 0 : b.dp[i - 1][k - currElement];
+                            int y = k - currElement < 0 || i - 1 <= 0 ? 0 : dp[i - 1][k - currElement];
                             dp[i][k] = (short) Integer.max(prevMax, currElement + y);
                         }
                     }
